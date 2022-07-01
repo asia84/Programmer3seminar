92a91d05464a846d9b72b420664c3153f0ed9cf0
Asia84 <84asia@mail.ru>
2022-07-01 20:57:10 +0700

    task18

---
+++ task18/Program.cs
@@ -0,0 +1,33 @@
+﻿/*18/Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти(x,y)*/
+
+int n;
+
+n = Convert.ToInt32(Console.ReadLine());
+if(n > 0 && n<5)
+{
+if(n ==1)
+{
+    Console.WriteLine("x = (0, +~)");
+    Console.WriteLine("y = (0, +~)");
+}   
+if( n == 2)
+{ 
+    Console.WriteLine("x = (-~, 0 )");
+    Console.WriteLine("y = (0, +~)");
+}
+if( n == 3)
+{
+    Console.WriteLine("x = (-~, 0 )");
+    Console.WriteLine("y = (-~, 0 )");
+}
+if( n ==  4)
+{ 
+    Console.WriteLine("x = (0, +~)");
+    Console.WriteLine("y = (-~, 0 )");
+}
+}
+else
+{
+    Console.WriteLine("Такой четверти нет!!! Ты врун!");
+}
