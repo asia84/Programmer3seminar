276506e631caa0132e0f8446f746f65f2ab22d39
Asia84 <84asia@mail.ru>
2022-07-01 21:27:51 +0700

    task21.1 рассчет расстояния в 3D)

---
+++ task21.1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*21(куб)Напишите программу, которая принимает на вход координаты двух точек
+ и находит расстояние между ними в 3D пространстве.*/
+
+double x, y, z, x1, y1, z1, distance;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+z = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+z1 = Convert.ToDouble(Console.ReadLine());
+distance = Math.Sqrt((x - x1) * (x - x1) + (y - y1 ) * (y - y1)) + (z-z1) * (z-z1); 
+Console.WriteLine(distance);  
+
